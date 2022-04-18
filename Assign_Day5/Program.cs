d5bfcb02bfd2998be26b30fb57f992b7d929c026
Komal <divyavairagade@gmail.com>
2022-04-18 14:27:10 +0530

    Swapping of two numbers

--- Assign_Day5/Program.cs
+++ Assign_Day5/Program.cs
@@ -2,8 +2,8 @@
 Console.WriteLine("Day 5 Assignment");
 //Assign_Day5.Class1.FlipCoin();
 //Assign_Day5.Class2.EvenOdd();
-Assign_Day5.Class3.LeapYear();
-//Assign_Day5.Class4.SwapNumbers();
+//Assign_Day5.Class3.LeapYear();
+Assign_Day5.Class4.SwapNumbers();
 //Assign_Day5.Class5.Harmonic();
 //Assign_Day5.Class6.VowelConsonant();
 //Assign_Day5.Class7.Power();
