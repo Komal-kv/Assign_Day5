7971a2cdea2f68a2aac0c0fd38f6bc6019dce429
Komal <divyavairagade@gmail.com>
2022-04-18 14:20:23 +0530

    flip the coin program added

--- Assign_Day5/Program.cs
+++ Assign_Day5/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Day 5 Assignment");
+Assign_Day5.Class1.FlipCoin();
+//Day5_PP.Class2.EvenOdd();
+//Day5_PP.Class3.LeapYear();
+//Day5_PP.Class4.SwapNumbers();
+//Day5_PP.Class5.Harmonic();
+//Day5_PP.Class6.VowelConsonant();
+//Day5_PP.Class7.Power();
+//Day5_PP.Class8.MaxNumber();
+//Day5_PP.Class9.PrimeFactorization();
+//Day5_PP.Class10.QuoRem();
