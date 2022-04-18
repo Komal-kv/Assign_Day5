cb4ee7b743c8872154ce66531dde5b38bfdef6eb
Komal <divyavairagade@gmail.com>
2022-04-18 14:22:57 +0530

    Even odd program added

--- Assign_Day5/Program.cs
+++ Assign_Day5/Program.cs
@@ -1,12 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Day 5 Assignment");
-Assign_Day5.Class1.FlipCoin();
-//Day5_PP.Class2.EvenOdd();
-//Day5_PP.Class3.LeapYear();
-//Day5_PP.Class4.SwapNumbers();
-//Day5_PP.Class5.Harmonic();
-//Day5_PP.Class6.VowelConsonant();
-//Day5_PP.Class7.Power();
-//Day5_PP.Class8.MaxNumber();
-//Day5_PP.Class9.PrimeFactorization();
-//Day5_PP.Class10.QuoRem();
+//Assign_Day5.Class1.FlipCoin();
+Assign_Day5.Class2.EvenOdd();
+//Assign_Day5.Class3.LeapYear();
+//Assign_Day5.Class4.SwapNumbers();
+//Assign_Day5.Class5.Harmonic();
+//Assign_Day5.Class6.VowelConsonant();
+//Assign_Day5.Class7.Power();
+//Assign_Day5.Class8.MaxNumber();
+//Assign_Day5.Class9.PrimeFactorization();
+//Assign_Day5.Class10.QuoRem();
